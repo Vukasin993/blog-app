34e3f2db2c6fed7ed6b58df4aac7295d09340206
Vukasin Macura <moj_email@gmail.com>
2020-10-09 18:44:23 +0200

    1. zadatak: Add new component and Posts View, add new link to page posts

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,14 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Posts from '../views/Posts.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home
+    path: '/posts',
+    name: 'posts',
+    component: Posts
   },
   {
     path: '/about',
